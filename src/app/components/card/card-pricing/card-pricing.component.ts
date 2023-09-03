a146cd0470ba99589bb346eb301ca0a697b9bf3c
Vinicius S. Lourenco <vinicius.vsl@gmail.com>
2023-09-02 21:29:19 -0300

    Created pricing component

--- src/app/components/card/card-pricing/card-pricing.component.ts
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-pricing',
   templateUrl: './card-pricing.component.html',
-  styleUrls: ['./card-pricing.component.css']
+  styleUrls: ['./card-pricing.component.css'],
 })
 export class CardPricingComponent {
-
+  @Input() gameType: string = '';
+  @Input() gamePrice: string = '';
 }
